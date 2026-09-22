53a7db6cc4be63271df4e3249ef599344662d4f1
Pablo-Almeida345 <pabloalmeidadasilva@gmail.com>
2026-09-22 19:55:57 -0300

    Inicio do projeto

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MeuCampusApp());
+}
+
+class MeuCampusApp extends StatelessWidget {
+  const MeuCampusApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Meu Campus'),
+        ),
+      ),
+    );
+  }
+}
